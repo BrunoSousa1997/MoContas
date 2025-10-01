a0e0bd5afbd631d0a462090b1f752875e785eaf1
BrunoSousa1997 <bruno-sousa-1997@hotmail.com>
2025-10-01 14:59:12 +0100

    App

---
+++ App/backend/controllers/income.controller.js
@@ -0,0 +1,21 @@
+const Income = require("../models/income.model");
+const mongoose = require("mongoose");
+
+module.exports = {
+    //Ver todos os Módulos
+    getIncomes: async (req, res) => {
+        const { month, year } = req.query;
+        const incomes = await Income.find({ user: req.params.id, month, year });
+        res.json(incomes);
+    },
+    newIncome: async (req, res) => {
+        const income = new Income(req.body);
+        await income.save();
+        res.json(income);
+    },
+    deleteIncome: async (req, res) => {
+        const { id } = req.params;
+        const incomes = await Income.findByIdAndDelete(id);
+        res.json(incomes);
+    },
+};
