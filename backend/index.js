082beca0cee6e2aae54a721f59ab749d997ac479
BrunoSousa1997 <bruno-sousa-1997@hotmail.com>
2025-11-16 09:43:31 +0000

    Correções

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,6 @@ require("dotenv").config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB connected"))
     .catch(err => console.log(err));
